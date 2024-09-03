f5225a711a6264a762f8ea26c436351be1548470
Marwa Mahmoud <marwa@bluskyint.com>
2024-09-03 12:52:23 +0300

    iltration [Restrecation] Operators-"Where"(deffered execuation)

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -11,9 +11,9 @@ namespace Demo
             ///Extention Methods For All Collection That Implement "IEnumrable" Interface 
             ///Named As Linq Operators Existed At Class Enumrable
             ///Categorized Into 13 Categorey
-            
+
             List<int> Numbers = new List<int>() { 1, 2, 5, 6, 7, 9, 12, 14, 15, 19 };
-            List<int> OddNumbers = Numbers.Where(x => x %2==1).ToList();
+            List<int> OddNumbers = Numbers.Where(x => x % 2 == 1).ToList();
             //foreach (int x in OddNumbers) { Console.WriteLine(x); }
             ///you can use "linq operator " Aganist the data that stored in sequence
             ///regardless data store
@@ -39,7 +39,7 @@ namespace Demo
             //from Numbers N
             //where N%2==1
             var oddNumbers = from N in Numbers01
-                             where N %2==1
+                             where N % 2 == 1
                              select N;
             //foreach (var number in oddNumbers) { Console.WriteLine(number); };
             //must begin with from and end with select or groupBy
@@ -57,14 +57,50 @@ namespace Demo
             #region immediate execuation[Elements Operators , Casting Operators , Aggregate Operators]
             List<int> Numbers02 = new List<int>() { 1, 2, 5, 6, 7, 9, 12, 14, 15, 19 };
             var evenNumbers = Numbers02.Where(x => x % 2 == 0).ToList();
-            Numbers02.AddRange(new List<int>() { 100, 101, 102, 103, 104 }); 
-           // foreach (var number in evenNumbers) { Console.WriteLine(number); };
+            Numbers02.AddRange(new List<int>() { 100, 101, 102, 103, 104 });
+            // foreach (var number in evenNumbers) { Console.WriteLine(number); };
             #endregion
             #endregion
             #region Set Up Data         
-            Console.WriteLine(ProductsList[0]);
-            Console.WriteLine(CustomersList[0]);
+            //Console.WriteLine(ProductsList[0]);
+            //Console.WriteLine(CustomersList[0]);
             #endregion
+            #region Filtration [Restrecation] Operators-"Where"(deffered execuation)
+            #region Get Products Out Of Stock[example 01]
+            //Get Products Out Of Stock
+            //fluent Syntax
+            //var Result = ProductsList.Where(x => x.UnitsInStock == 0);
+            //query syntax
+            //  var Result = from N in ProductsList
+            //           where N.UnitsInStock==0
+            //         select N;
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+            #region Get Products in  Stock & in categorey of meat/poultry[example 02]
+            //fluent Syntax
+            // var Result = ProductsList.Where(x => x.UnitsInStock>0 && x.Category== "Meat/Poultry");
+            //query syntax
+            //Result = from N in ProductsList
+            //         where N.UnitsInStock > 0 && N.Category == "Meat/Poultry"
+            //          select N;
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+            #region Indexed Where[valid only with fluent syntax ]
+            //fluent Syntax
+            var Result = ProductsList.Where((x, i) => x.UnitsInStock > 0 && i < 9);
+            foreach (var item in Result)
+            {
+                Console.WriteLine(item);
+            }
+            #endregion 
+            #endregion
+
         }
     }
 }
